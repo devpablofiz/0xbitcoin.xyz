3afd31b3f3e41fbf76d2ae24bd2757ee1f3c92d7
Biagio00 <francescobiagioni00@gmail.com>
2022-04-30 14:30:15 +0200

    Relative to center rock hitbox

--- halvening-website/httpsgameserver.js
+++ halvening-website/httpsgameserver.js
@@ -184,7 +184,7 @@ for(const [rockIndex, rockValue] of Object.entries(rockData)) {
     // });
     rockBodies[rockIndex] = rockBody;
     //body shape initialization
-    const rockShape = planck.Box(mapGridSize / 2, mapGridSize / 2); //created with half extents
+    const rockShape = planck.Box(mapGridSize / 2, mapGridSize / 2, planck.Vec2(0, -4 / scale), 0 * Math.PI); //created with half extents, second parameter is the position relative to the center of the body, third parameter the rotation
 
     const rockFixiture = rockBody.createFixture({ //non importa salvare playerFixiture
         shape: rockShape,
